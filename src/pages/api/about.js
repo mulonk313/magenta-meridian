ec7382be1d11200979f04acb93ab7cc4d67f46d3
John Karounis <j.karounis96@gmail.com>
2024-12-03 13:12:44 +0200

    Initializing tailwind css and delete pre files

---
+++ src/pages/api/about.js
@@ -0,0 +1,8 @@
+export async function get() {
+    return {
+      body: JSON.stringify({
+        description: 'Ασχολούμαι με την ανάπτυξη ιστοσελίδων και στοχεύω στη δημιουργία καινοτόμων projects.'
+      }),
+    };
+  }
+  
